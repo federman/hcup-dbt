3f2cc0d3226849c5fb6d1c52b44a17edc35538c4
rl627 <45013044+rl627@users.noreply.github.com>
2023-01-11 16:58:03 -0500

    abstract `ili_diagnosis_var` away from mart model into a CTE in the intermediate model #4

--- models/marts/flu/line_level/mart__er_flu__line_level.sql
+++ models/marts/flu/line_level/mart__er_flu__line_level.sql
@@ -10,11 +10,6 @@ select
   ZIP,
   flu,
   flu_like,
-  CASE
-    WHEN (flu AND flu_like) THEN 'BOTH'
-    WHEN (flu AND NOT(flu_like)) THEN 'ILI'
-    WHEN (NOT(flu) AND flu_like) THEN 'ILI LIKE'
-    ELSE 'OTHER'
-    END AS ili_diagnosis_var
+  ili_diagnosis_var
 FROM {{ref('int_sedd_flu_line_level')}}
 
