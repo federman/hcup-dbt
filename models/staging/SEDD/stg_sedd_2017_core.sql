1c7765b0f09080e20475c14eb52352184902553f
rl627 <45013044+rl627@users.noreply.github.com>
2023-01-12 13:56:13 -0500

    added `db_file` to all stg_ models #4

--- models/staging/SEDD/stg_sedd_2017_core.sql
+++ models/staging/SEDD/stg_sedd_2017_core.sql
@@ -14,6 +14,7 @@ SELECT
   I10_DX_Visit_Reason2,
   I10_DX1,
   I10_DX2,
+  'sedd_core' AS db_file,
   CONCAT_WS('', AYEAR, '-', AMONTH, '-01') AS admit_date,
 FROM {{ source('SEDD', 'NY_SEDD_2017_CORE') }}
 
