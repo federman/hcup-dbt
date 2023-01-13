8e04f3be45b9bb616abc53708930b2444969adea
rl627 <45013044+rl627@users.noreply.github.com>
2023-01-13 16:39:12 -0500

    renamed n to count in zcta level models

--- models/marts/flu/zip_level/mart__er_flu__zip_level.sql
+++ models/marts/flu/zip_level/mart__er_flu__zip_level.sql
@@ -8,7 +8,7 @@ SELECT
   flu,
   flu_like,
   ili_diagnosis_var,
-  COUNT(*) AS n
+  COUNT(*) AS count
 FROM {{ref('mart__er_flu__line_level')}}
 GROUP BY AGE, RACE, HISPANIC, ZIP, FEMALE, flu, flu_like, ili_diagnosis_var
 
