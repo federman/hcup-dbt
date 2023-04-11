5baf88f34f1a10e667f09e493148dfe4b14055da
rl627 <45013044+rl627@users.noreply.github.com>
2023-04-11 19:59:02 -0400

    operationalize RUCA, ZCTA and urban in int__side_core

--- models/intermediate/int__sid_core.sql
+++ models/intermediate/int__sid_core.sql
@@ -1,7 +1,7 @@
 {{ config(materialized='external', format =  target.schema) }}
 with 
 
-staged as (
+cte_1 as (
       select * from {{ref('base__az_sid_2016_core')}}
       union
       select * from {{ref('base__az_sid_2017_core')}}
@@ -27,12 +27,62 @@ staged as (
       select * from {{ref('base__ny_sid_2018_core')}}
 ),
 
-final as (
+cte_2 as (
   select 
   *,
   {{recode_race_ethnicity()}} AS race_ethnicity, 
   {{recode_insurance()}} AS insurance, 
-  from staged
+  from cte_1
+  left join {{ref('xwalk_zip_urban')}} as xwalk_zip_urban
+  on cte_1.ZIP = xwalk_zip_urban.ZIP
+  left join {{ref('xwalk_zip_zcta')}} as xwalk_zip_zcta
+  on cte_1.ZIP = xwalk_zip_zcta.ZIP
+
+),
+
+final as (
+  select 
+    AGE,
+    AMONTH,
+    AYEAR,
+    CPT1,
+    CPT2,
+    CPT3,
+    CPTCCS1,
+    CPTCCS2,
+    DHOUR,
+    DIED,
+    DISP_X,
+    DISPUB04,
+    DMONTH,
+    DQTR,
+    DRG,
+    DSHOSPID,
+    FEMALE,
+    HCUP_ED,
+    HCUP_OS,
+    HISPANIC,
+    HOSP_NPI,
+    HOSPST,
+    I10_DX_Admitting,
+    I10_DX1,
+    I10_DX2,
+    KEY,
+    LOS,
+    PAY1,
+    PSTCO,
+    PSTCO2,
+    RACE,
+    VisitLink,
+    ZIP,
+    ZIP3,
+    ZIPINC_QRTL,
+    db,
+    file,
+    RUCA,
+    urban,
+    ZCTA,
+  from cte_2
 )
 
 select * from final
