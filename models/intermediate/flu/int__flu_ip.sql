62b9dd94b882e3d9322c84bb95d55cadeadab298
rl627 <45013044+rl627@users.noreply.github.com>
2023-04-11 13:23:42 -0400

    drafted `int__flu_ip.sql` #19

---
+++ models/intermediate/flu/int__flu_ip.sql
@@ -0,0 +1,51 @@
+{{ config(materialized='external', format =  target.schema) }}
+with 
+
+staged as (
+  select * from {{ref('base__az_sid_2016_core')}}
+  union
+  select * from {{ref('base__az_sid_2017_core')}}
+  union
+  select * from {{ref('base__az_sid_2018_core')}}
+  union
+  select * from {{ref('base__az_sid_2019_core')}}
+  union
+  select * from {{ref('base__ga_sid_2016_core')}}
+  union
+  select * from {{ref('base__ga_sid_2017_core')}}
+  union
+  select * from {{ref('base__ga_sid_2018_core')}}
+  union
+  select * from {{ref('base__ga_sid_2019_core')}}
+  union
+  select * from {{ref('base__nj_sid_2016_core')}}
+  union
+  select * from {{ref('base__nj_sid_2017_core')}}
+  union
+  select * from {{ref('base__ny_sid_2017_core')}}
+  union
+  select * from {{ref('base__ny_sid_2018_core')}}
+),
+
+int as (
+  select 
+  *,
+  {{dx_flag_flu()}} AS flu,
+  {{dx_flag_flu_like()}} AS flu_like, 
+  from staged
+),
+
+final as (
+  select 
+  *,
+  CASE
+    WHEN (flu AND flu_like) THEN 'BOTH'
+    WHEN (flu AND NOT(flu_like)) THEN 'ILI'
+    WHEN (NOT(flu) AND flu_like) THEN 'ILI LIKE'
+    ELSE 'OTHER'
+    END AS ili_diagnosis_var  
+  from int
+)
+
+select * from final
+
