002a21b2883ce0ee9bca5f432bdc43b1921203ec
rl627 <45013044+rl627@users.noreply.github.com>
2023-04-11 17:00:36 -0400

    oeprationalize `int__sid_chgs` #20

---
+++ models/intermediate/int__sid_chgs.sql
@@ -0,0 +1,38 @@
+{{ config(materialized='external', format =  target.schema) }}
+with 
+
+staged as (
+      select * from {{ref('base__az_sid_2016_chgs')}}
+      union
+      select * from {{ref('base__az_sid_2017_chgs')}}
+      union
+      select * from {{ref('base__az_sid_2018_chgs')}}
+      union
+      select * from {{ref('base__az_sid_2019_chgs')}}
+      union
+      select * from {{ref('base__ga_sid_2016_chgs')}}
+      union
+      select * from {{ref('base__ga_sid_2017_chgs')}}
+      union
+      select * from {{ref('base__ga_sid_2018_chgs')}}
+      union
+      select * from {{ref('base__ga_sid_2019_chgs')}}
+      union
+      select * from {{ref('base__nj_sid_2016_chgs')}}
+      union
+      select * from {{ref('base__nj_sid_2017_chgs')}}
+      union
+      select * from {{ref('base__ny_sid_2017_chgs')}}
+      union
+      select * from {{ref('base__ny_sid_2018_chgs')}}
+),
+
+final as (
+  select 
+  KEY,
+  CHARGE
+  from staged
+)
+
+select * from final
+
