3bdb4e71eeff7d71f91992c33818425f234987a5
rl627 <45013044+rl627@users.noreply.github.com>
2023-04-11 15:30:35 -0400

    refactor `mart__flu_ip_urban_rh` for readability

--- models/mart/flu/IP Encounter/mart__flu_ip_urban_rh.py
+++ models/mart/flu/IP Encounter/mart__flu_ip_urban_rh.py
@@ -5,14 +5,12 @@ def model(dbt, session):
     ## Define the model config
     dbt.config(materialized = "external", format = 'parquet' )
 
-    ## Read in intermediate table
-    df_intermediate = pl.from_arrow(dbt.ref("int__flu_ip").arrow()) 
-     
-    ## Read in ZIP to Urban Crosswalk
+    ## Read in upstream tables
+    df_int__flu_ip = pl.from_arrow(dbt.ref("int__flu_ip").arrow()) 
     xwalk_zip_urban = pl.from_arrow(dbt.ref("xwalk_zip_urban").arrow())
 
     ## Mart Items Logic
-    df_final = (df_intermediate
+    df_final = (df_int__flu_ip
       .select(['KEY', 'ZIP','race_ethnicity', 'ili_diagnosis_var']) 
       .join(xwalk_zip_urban, on = 'ZIP', how = 'left')     
       .groupby(['ili_diagnosis_var','race_ethnicity','urban'])
