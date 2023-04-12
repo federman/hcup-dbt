c4c4f81c2b02d740f17b84653eb3046774e80c2f
rl627 <45013044+rl627@users.noreply.github.com>
2023-04-11 21:23:54 -0400

    operationalize `mean_chrg_per_encounter` in  mart__flu_ip_cost_city #20

--- models/mart/flu/IP Cost/mart__flu_ip_cost_city.py
+++ models/mart/flu/IP Cost/mart__flu_ip_cost_city.py
@@ -8,25 +8,23 @@ def model(dbt, session):
     ## Read in upstream tables
     df_int__flu_ip = pl.from_arrow(dbt.ref("int__flu_ip").arrow()) 
     df_int__sid_chgs = pl.from_arrow(dbt.ref("int__sid_chgs").arrow()) 
-    xwalk_zip_zcta = pl.from_arrow(dbt.ref("xwalk_zip_zcta").arrow()) 
     xwalk_zcta_place = pl.from_arrow(dbt.ref("xwalk_zcta_place").arrow()) 
 
     
     ## Operationalize KEY to ili_indicator crosswalk
     key_metadata =  (df_int__flu_ip
-      .select(['KEY','AYEAR','AMONTH', 'ZIP', 'ili_diagnosis_var'])  
+      .select(['KEY','AYEAR','AMONTH', 'ZIP','ZCTA', 'ili_diagnosis_var'])  
       .unique())
 
     ## Transformations
     df_final = (df_int__sid_chgs
                  .select(['KEY','CHARGE']) 
-                 ##.drop_nulls(subset=['CHARGE'])    
                  .join(key_metadata, on = 'KEY', how = 'left')
-                 ##.drop_nulls(subset=['ZIP']) 
-                 .join(xwalk_zip_zcta, on = 'ZIP', how = 'left') 
                  .join(xwalk_zcta_place, on = 'ZCTA', how = 'left') 
-                 ##.groupby('PLACE')
-                 ##.agg({'CHARGE': 'mean'})
+                 .filter(pl.col('PLACE').is_not_null())
+                 .groupby(['AYEAR', 'AMONTH', 'ili_diagnosis_var', 'PLACE'])
+                 .agg(pl.mean('CHARGE').alias('mean_chrg_per_encounter'))
+                 .sort(by=['AYEAR', 'AMONTH', 'ili_diagnosis_var', 'mean_chrg_per_encounter'])
                  .to_pandas())
 
     return df_final
