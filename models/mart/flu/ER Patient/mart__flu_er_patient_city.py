a0d7f1e51bc1cac734220b837e4f4442b324af45
rl627 <45013044+rl627@users.noreply.github.com>
2023-04-11 23:37:13 -0400

    generate `mart__flu_er_patient_city` model #18

---
+++ models/mart/flu/ER Patient/mart__flu_er_patient_city.py
@@ -0,0 +1,23 @@
+import pyarrow as pa
+import polars as pl
+
+def model(dbt, session):
+    ## Define the model config
+    dbt.config(materialized = "external", format = 'csv' )
+
+    ## Read in upstream tables
+    er_encounters = pl.from_arrow(dbt.ref("int__flu_er").arrow()) 
+    xwalk_zcta_place = pl.from_arrow(dbt.ref("xwalk_zcta_place").arrow()) 
+
+    ## Transformations
+    mart = (er_encounters
+                .select(['AYEAR', 'AMONTH','ZCTA','ili_diagnosis_var','VisitLink']) 
+                .join(xwalk_zcta_place, on = 'ZCTA', how = 'left') 
+                .select(['AYEAR', 'AMONTH', 'ili_diagnosis_var', 'PLACE', 'VisitLink']) 
+                .drop_nulls(subset=["VisitLink"])
+                .unique()
+                .groupby(['AYEAR', 'AMONTH', 'ili_diagnosis_var', 'PLACE'])
+                .agg(pl.count().alias('n_unique_er_patients'))
+                .sort(by=['AYEAR', 'AMONTH', 'ili_diagnosis_var', 'n_unique_er_patients'])
+                .to_pandas())
+    return mart
