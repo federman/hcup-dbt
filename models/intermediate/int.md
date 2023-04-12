e0e36cb40613117468073171805e2c24518c3834
rl627 <45013044+rl627@users.noreply.github.com>
2023-04-11 22:07:03 -0400

    draft `int__sedd_core` 17

--- models/intermediate/int.md
+++ models/intermediate/int.md
@@ -1,9 +1,16 @@
+
+
+{% docs int__sedd_core_description %}
+
+This intermediate table compiles all in emergency room encounter records from SEDD CORE files across years and states into a single table. Then conducts some additional merges to assign some spatial metadata to each record. The result is a **single table which contains all ER encounters and associated encounter-level metadata.**.
+
+{% enddocs %}
+
 {% docs int__sid_core_description %}
 
 This intermediate table compiles all in patient discharge records from SID CORE files across years and states into a single table. Then conducts some additional merges to assign some spatial metadata to each record. The result is a **single table which contains all encounters and associated encounter-level metadata.**.
 
 {% enddocs %}
-
 
 {% docs int__sid_chgs_description %}
 
---
+++ models/intermediate/int__sedd_core.sql
@@ -0,0 +1,73 @@
+{{ config(materialized='external', format =  target.schema) }}
+with 
+
+cte_1 as (
+      select * from {{ref('base__ny_sedd_2017_core')}}
+      union
+      select * from {{ref('base__ny_sedd_2018_core')}}
+),
+
+cte_2 as (
+  select 
+  *,
+  from cte_1
+  left join {{ref('xwalk_zip_urban')}} as xwalk_zip_urban
+  on cte_1.ZIP = xwalk_zip_urban.ZIP
+  left join {{ref('xwalk_zip_zcta')}} as xwalk_zip_zcta
+  on cte_1.ZIP = xwalk_zip_zcta.ZIP
+),
+
+cte_3 as (
+  select 
+    AGE,
+    CPT1,
+    CPT2,
+    CPT3,
+    CPTCCS1,
+    CPTCCS2,
+    DHOUR,
+    DIED,
+    DISP_X,
+    DISPUB04,
+    DMONTH,
+    DQTR,
+    DSHOSPID,
+    FEMALE,
+    HCUP_ED,
+    HCUP_OS,
+    HISPANIC,
+    HOSP_NPI,
+    HOSPST,
+    I10_DX_Visit_Reason1,
+    I10_DX_Visit_Reason2,
+    I10_DX1,
+    I10_DX2,
+    KEY,
+    LOS,
+    PAY1,
+    PSTCO,
+    PSTCO2,
+    RACE,
+    VisitLink,
+    YEAR,
+    ZIP,
+    ZIP3,
+    ZIPINC_QRTL,
+    db,
+    file,
+    RUCA,
+    urban,
+    ZCTA
+  from cte_2
+),
+
+final as (
+  select 
+  *,
+  {{recode_race_ethnicity()}} AS race_ethnicity, 
+  {{recode_insurance()}} AS insurance, 
+  from cte_3
+)
+
+select * from final
+
